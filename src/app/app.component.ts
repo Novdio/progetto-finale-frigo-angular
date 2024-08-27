32d111e9ff9ec14857ec54e05a28b5dfb6def566
Andrea Paffi <andreapaffi2002@gmail.com>
2024-08-27 14:40:33 +0200

    in italiano

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
-import { ProvaComponent } from "./carosello/prova.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ProvaComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
